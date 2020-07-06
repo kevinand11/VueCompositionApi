b2271658d0ec77ed9cdffbf655de5fd0b55f714e
Izuchukwu Kevin <kevinfizu@gmail.com>
2020-07-06 20:28:07 +0100

    Removed store plugin and added composition api plugin

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+
+import CompositionAPi from '@vue/composition-api'
+
+Vue.use(CompositionAPi)
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	render: (h) => h(App),
 }).$mount('#app')
